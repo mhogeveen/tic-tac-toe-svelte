57524651e72e8a48a3233a54fa33e4e0b8c3cd1b
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-07-08 16:45:45 +0200

    Add icon options to icon component

--- src/components/Icon.svelte
+++ src/components/Icon.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-  import { Times, Circle } from './icons';
+  import { Times, Circle, Award, Repeat } from './icons';
   import type { SvelteComponent } from 'svelte';
 
   export let icon: string;
   export let color: string = '#000';
   export let size: string = '20px';
+  export let classes: string = '';
 
   interface Icons {
     [key: string]: typeof SvelteComponent;
@@ -13,7 +14,9 @@
   const icons: Icons = {
     'x': Times,
     'o': Circle,
+    'a': Award,
+    'r': Repeat
   }
 </script>
 
-<svelte:component this={ icons[icon] } {color} {size} />
+<svelte:component this={ icons[icon] } {color} {size} {classes} />
