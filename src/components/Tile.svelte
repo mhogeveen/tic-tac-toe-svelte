cc51c111fa8d6246997db7a3b87ec45303fd333c
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-07-08 16:45:58 +0200

    Add logic to tile component

--- src/components/Tile.svelte
+++ src/components/Tile.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
-  import { gridData } from '$stores';
+  import { gridData, winState } from '$stores';
   import { Icon } from '$components';
+  import { hasWon } from '$utils';
 
   export let player: string | null;
   export let index: number;
+
+  const onTileClick = () => {
+    gridData.setTilePlayer(index)
+    const winStateCheck = hasWon($gridData)
+    if (winStateCheck.gameOver) {
+      winState.setWinners(winStateCheck.winningTiles)
+    }
+  }
 </script>
 
 <div
-  class="h-[15vw] max-h-40 w-[15vw] max-w-[10rem] bg-dark grid place-items-center outline-4 outline-transparent outline-offset-[-8px] outline {!player ? 'hover:outline-green' : ''} transition-all"
+  class="h-[15vw] max-h-40 w-[15vw] max-w-[10rem] bg-dark grid place-items-center outline-4 outline-transparent outline-offset-[-8px] outline transition-all"
   class:cursor-pointer={!player}
-  on:click={() => gridData.setTilePlayer(index)}
+  class:hover:outline-green={!player}
+  class:outline-green={$winState?.includes(index)}
+  class:pointer-events-none={$winState?.length}
+  on:click={onTileClick}
 >
   {#if player}
     <Icon icon={player} size={'50%'} color={'#fff'} />
