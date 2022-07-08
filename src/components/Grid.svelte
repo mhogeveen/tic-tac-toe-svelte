77f9ed1a5280eb8130a8b5b2939a0e222f483005
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-07-08 16:45:34 +0200

    Add current player display to grid

--- src/components/Grid.svelte
+++ src/components/Grid.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
-  import { Tile, Button } from '$components';
-  import { gridData } from '$stores';
+  import { Tile, Button, Icon } from '$components';
+  import { gridData, winState, currentPlayer } from '$stores';
+
+  const onResetClick = () => {
+    gridData.reset()
+    winState.reset()
+  }
 </script>
+
+<div class="flex justify-center items-center gap-2 h-[15vw] max-h-40 w-[30vw] max-w-[20rem]">
+  {#if !$winState?.length}
+    <Icon icon={'x'} size={'50px'} color={$currentPlayer === 'x' ? '#10b981' : '#e11d48'} />
+    <Icon icon={'o'} size={'50px'} color={$currentPlayer === 'o' ? '#10b981' : '#e11d48'} />
+  {:else}
+    <Icon icon={$currentPlayer === 'x' ? 'o' : 'x'} size={'50px'} color={'#fff'} />
+    <Icon icon={'a'} size={'50px'} color={'#10b981'} />
+  {/if}
+</div>
 
 <div class="grid grid-cols-3 gap-1 bg-teal">
   {#each $gridData as { player, index } (index)}
@@ -9,4 +24,6 @@
   {/each}
 </div>
 
-<Button text={'reset'} on:click={() => gridData.reset()} />
+<Button on:click={onResetClick} >
+  <Icon slot="before" icon={'r'} size={'50px'} color={'#fff'} classes={"hover:rotate-180 transition-transform"} />
+</Button>
