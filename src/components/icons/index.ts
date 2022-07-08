b1ddfbe58905a2ae63b2af04b2cef971be57c755
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-07-08 16:42:02 +0200

    Add icons

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,7 +1,11 @@
 import Circle from './Circle.svelte';
 import Times from './Times.svelte';
+import Award from './Award.svelte';
+import Repeat from './Repeat.svelte';
 
 export {
   Circle,
   Times,
+  Award,
+  Repeat
 }
